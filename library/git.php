8fd5ae81794538d3b52028fccc091fd59b2d68e7
djtaylor <djtaylor@mba>
2013-08-31 15:02:59 +0700

    Strip out file headers

--- library/git.php
+++ library/git.php
@@ -6,8 +6,11 @@ function git_code($path) {
 	// Define the source GitHub branch
 	$branch = 'dev';
 	
-	// Get the raw file contents and return them
-	$contents = file_get_contents("https://raw.github.com/workvm/intengine/$branch/$path");
+	// Get the raw file contents
+	$contents_raw = file_get_contents("https://raw.github.com/workvm/intengine/$branch/$path");
+	
+	// Strip out the file header and return the contents
+	$contents = preg_replace('/^#.*[Info\]*[Info\/]*$/', '', $contents_raw);
 	echo $contents;
 	
 }
