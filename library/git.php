c7a94dfabfca4fbe353dc4a103ee2eca11fd1085
djtaylor <djtaylor@mba>
2013-08-31 18:54:46 +0700

    still need to fix regex

--- library/git.php
+++ library/git.php
@@ -12,7 +12,7 @@ function git_code($path) {
 	// Strip out the file header
 	$delete_lines = false;
 	foreach ($contents as $line_num => $line) {
-		if (preg_match("/Info\\\/i", $line)) {
+		if (preg_match("/Info\/i", $line)) {
 			unset($contents[$line_num]);
 			$delete_lines = true;
 		}
