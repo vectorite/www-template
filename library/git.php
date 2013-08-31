c057111edfbd2842eba17a5365081aedfd547122
djtaylor <djtaylor@mba>
2013-08-31 18:52:40 +0700

    trying something else

--- library/git.php
+++ library/git.php
@@ -6,12 +6,29 @@ function git_code($path) {
 	// Define the source GitHub branch
 	$branch = 'dev';
 	
-	// Get the raw file contents
-	$contents_raw = file_get_contents("https://raw.github.com/workvm/intengine/$branch/$path");
+	// Get the file contents
+	$contents = file("https://raw.github.com/workvm/intengine/$branch/$path");
 	
-	// Strip out the file header and return the contents
-	$contents = preg_replace('/^[^\n]*Info\\\.+?Info\/[^\n]*$/s', '', $contents_raw);
-	echo $contents;
+	// Strip out the file header
+	$delete_lines = false;
+	foreach ($contents as $line_num => $line) {
+		if (preg_match("/Info\\\/i", $line)) {
+			unset($contents[$line_num]);
+			$delete_lines = true;
+		}
+		if ($delete_lines = true) {
+			unset($contents[$line_num]);
+		}
+		if (preg_match("/Info\//i", $line)) {
+			unset($contents[$line_num]);
+			break;
+		}
+	}
+	
+	// Return the file contents
+	foreach ($contents as $line) {
+		echo $line;
+	}
 	
 }
 
