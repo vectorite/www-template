f5f9149dfc4032111a25b75ecff3708ff3900b7b
djtaylor <djtaylor@mba>
2013-09-01 11:13:00 +0700

    trying again to strip out header

--- library/git.php
+++ library/git.php
@@ -6,13 +6,12 @@ function git_code($path) {
 	// Define the source GitHub branch
 	$branch = 'dev';
 	
-	// Get the file contents
-	$contents = file("https://raw.github.com/workvm/intengine/$branch/$path");
+	// Get the file contents and strip the header
+	$contents = get_file_contents("https://raw.github.com/workvm/intengine/$branch/$path");
+	$contents = preg_replace('/^\#\s\[Info\\\\].*\#\s\[Info\/\]$/s', '', $contents);
 	
 	// Return the file contents
-	foreach ($contents as $line) {
-		echo $line;
-	}
+	echo $contents;
 	
 }
 
