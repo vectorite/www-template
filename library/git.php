236a6397b40c1b84cadf3800a66333f614ffb38b
djtaylor <djtaylor@mba>
2013-09-01 11:51:34 +0700

    this might work

--- library/git.php
+++ library/git.php
@@ -6,9 +6,24 @@ function git_code($path) {
 	// Define the source GitHub branch
 	$branch = 'dev';
 	
-	// Get the file contents and strip the header
-	$contents = file_get_contents("https://raw.github.com/workvm/intengine/$branch/$path");
-	$contents = preg_replace("/(#\!\/bin\/bash).*# \[Info\\.*Info\/\]/s", "$1", $contents);
+	// Get the file contents
+	$contents = file("https://raw.github.com/workvm/intengine/$branch/$path");
+	
+	// Strip the header
+	$delete_lines = false;
+	foreach ($contents as $line_num => $line) {
+		if (preg_match("/^#.*Info\\/", $line)) {
+			unset($contents[$line_num]);
+			$delete_lines = true;
+		}
+		if (preg_match("/^#.*Info\//", $line)) {
+			unset($contents[$line_num]);
+			$delete_lines = false;
+		}
+		if ($delete_lines = true) {
+			unset($contents[$line_num]);
+		}
+	}
 	
 	// Return the file contents
 	echo $contents;
