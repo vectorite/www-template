2834af8c218175c94dd3a9e678cc5fbf17ee405a
djtaylor <djtaylor@mba>
2013-09-01 11:04:52 +0700

    trying to fix git source pull

--- library/git.php
+++ library/git.php
@@ -9,22 +9,6 @@ function git_code($path) {
 	// Get the file contents
 	$contents = file("https://raw.github.com/workvm/intengine/$branch/$path");
 	
-	// Strip out the file header
-	$delete_lines = false;
-	foreach ($contents as $line_num => $line) {
-		if (strpos($line,'[Info\]') !== false) {
-   			unset($contents[$line_num]);
-			$delete_lines = true;
-		}
-		if (strpos($line,'[Info/]') !== false) {
-			unset($contents[$line_num]);
-			break;
-		}
-		if ($delete_lines = true) {
-			unset($contents[$line_num]);
-		}
-	}
-	
 	// Return the file contents
 	foreach ($contents as $line) {
 		echo $line;
